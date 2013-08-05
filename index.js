f0d12a634119e9f9e3d1923cec365fa8b8ddd148
Shallker Wang <shallker.wang@profero.com>
2013-08-05 18:50:40 +0800

    Coffeeify index.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('./lib/liverefresh');
+var fs = require('fs');
+var CoffeeScript = require('coffee-script');
+
+var coffeeFile = __dirname + '/index.coffee';
+
+CoffeeScript.run(
+  fs.readFileSync(coffeeFile, {encoding: 'utf8'}),
+  {filename: coffeeFile}
+);
