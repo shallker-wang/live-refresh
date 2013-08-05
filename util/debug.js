4746a5d4052680ba3c28f9ef7ad8740cb5a9875d
Shallker Wang <shallker.wang@profero.com>
2013-08-05 23:24:25 +0800

    Update util debug.

--- util/debug.js
+++ util/debug.js
@@ -6,35 +6,24 @@ debug.output = {
   3: true
 }
 
-debug.log = function(masterName, level) {
-  if (typeof level === 'undefined') {
-    level = 1;
+module.exports = exports = debug;
+
+exports.log = function(masterName, outputLevel) {
+  if (typeof outputLevel === 'undefined') {
+    outputLevel = 1;
   }
   return function() {
-    if (!debug.output[level]) return;
-    var argumentsArray = Array.prototype.slice.call(arguments);
-    argumentsArray.unshift(masterName);
-    console.log.apply(console, argumentsArray);    
+    if (!debug.output[outputLevel]) return;
+    var args = Array.prototype.slice.call(arguments);
+    args.unshift(masterName);
+    console.log.apply(console, args);
   }
 }
 
-debug.set = function(name, value) {
-  if (typeof debug.set[name] === 'function') return debug.set[name](value);
-  debug[name] = value;
-  return debug;
-}
-
-debug.get = function(name) {
-  if (typeof debug.get[name] === 'function') return debug.get[name](name);
-  return debug[name];
-}
-
-debug.error = function(masterName) {
+exports.error = function(masterName) {
   return function(message) {
     err = new Error(message)
     err.from = masterName
     throw err
   }
 }
-
-module.exports = debug;
