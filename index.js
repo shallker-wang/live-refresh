5dc640609997d537dcdc381cd26e0b1177ee76ad
Shallker Wang <shallker.wang@profero.com>
2013-08-07 14:43:03 +0800

    Version 1.1.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("coffee-script")
 var liverefresh = require('./lib/liverefresh')
 
-exports.version = '1.1.0';
+exports.version = '1.1.1';
 
 exports.refresh = function(path) {
   path = path || './';
